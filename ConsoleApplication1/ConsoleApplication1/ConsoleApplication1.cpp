0e2c9d8180d4e10fd87a23de94129a2d368e2700
Kirill.Smoktii <Smoktii-iGov@github.com>
2017-04-26 09:47:08 +0300

    My commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+{	
+	int i;
+	setlocale(LC_ALL, "RUS");
+
+	cout << "Здравствуй, Мир!\n";
+	system("pause");
+	return 0;
+}
+
